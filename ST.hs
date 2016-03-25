703227674bed17240ef138bdc0ea5c86d930919f
Mike Simister <mikesimister@shaw.ca>
2016-03-24 21:44:16 -0600

    messy.... needs a monad? a maybe type?

--- ST.hs
+++ ST.hs
@@ -17,7 +17,7 @@ data SYM_I_DESC = I_VARIABLE (Int,Int,M_type,Int)
 			  | I_TYPE [String];
 			  
 data ScopeType = L_PROG 
-              | L_FUN M_Type 
+              | L_FUN M_type 
               | L_BLK 
               | L_CASE
               
@@ -28,55 +28,7 @@ data SYM_VALUE = Var_attr (Int,M_type,Int)
               | Typ_attr [String]
 
 
-data SYM_TABLE = Symbol_table (Int,Int,[(String,SYM_VALUE)])
+data SYM_TABLE = Symbol_table (ScopeType,Int,Int,[(String,SYM_VALUE)])
 
 type ST = [SYM_TABLE]
 
-{-
-insert :: Int -> ST -> SYM_DESC -> (Int,ST)
-insert n [] d =  error "Symbol table error: insertion before defining scope."
-insert n ((Symbol_table(nL,nA,sL)):rest)(ARGUMENT(str,t,dim)) 
-	   | (in_index_list str sL) = error ("Symbol table error: " ++ str ++"is already defined.")
-	   | otherwise = (n,Symbol_table(nL,nA+1,(str,Var_attr(~(nA+4),T,dim))::sL))
-insert n ((Symbol_table(nL,nA,sL)):rest) (VARIABLE (str,T,dim)) 
-	   | (in_index_list str sL) = error ("Symbol table error: "++ str ++"is already defined.")
-	   | otherwise = (n,Symbol_table(nL+1,nA,(str,Var_attr(nL+1,T,dim))::sL))
-insert n ((Symbol_table(nL,nA,sL)):rest) FUNCTION (str,Ts,T)
-	   | in_index_list str sL = error ("Symbol table error: "++str++"is already defined.")
-	   | otherwise = (n+1,(Symbol_table(nL,nA,(str,Fun_attr(getlabel n "fn",Ts,T)):sL)):rest)
-....
-
-where 
-in_index_list str [] = False
-in_index_list str ((x,_):xs)| str==x = True
-                            | otherwise = in_index_list str xs
-
-
-lookup: ST -> String -> SYM_I_DESC
-look_up s x = find 0 s where
-found level (Var_attr(offset,type,dim)) =  I_VARIABLE(level,offset,type,dim)
-found level (Fun_attr(label,arg_Type,type)) = I_FUNCTION(level,label,arg_Type,type)
-      ...
-
-find_level ((str,v):rest)|x== str = Just v
-					   |otherwise =  find_level rest
-find_level [] = Nothing
-
-find n [] = error ("Could not find "++ str)
-find n (Symbol_table(_,_,vs)::rest) = 
-	 (case find_level vs of 
-	  Just v -> found n v
-  Nothing -> find (n+1) rest)
-
-empty:: ST
-empty = []
-
-
-new_scope :: ScopeType -> ST -> ST
-newscope s = (Symbol_table(0,0,[])):s
-
-removeScope :: ScopeType -> ST -> ST
-
- 
-return :: ST -> M_type
--}
--- SymbolTable.hs
+++ SymbolTable.hs
@@ -2,21 +2,100 @@
 module SymbolTable (ST,empty,new_scope,insert,lookup,return)
 where 
 import ST
+
    
 empty :: ST 
 empty = []
   
 new_scope:: ScopeType -> ST -> ST
-newscope s (st(a,b,[])) = (Symbol_table(a+1,b+1,[])):s
+new_scope s xs = Symbol_table(s,0,0,[]):xs
 
-insert:: Int -> ST -> SYM_DESC -> (Int,ST)
-insert --ARGUMENT (String,M_type,Int)
-insert --              | VARIABLE (String,M_type,Int)
-insert --              | FUNCTION (String,[(M_type,Int)],M_type)
-insert--              | DATATYPE String 
-insert --              | CONSTRUCTOR (String, [M_type], String)
+--inserts a value into the symbol table
+insert :: Int -> ST -> SYM_DESC -> (Int,ST)              
+insert n [] d = (n, error "Symbol table error: insertion before defining scope.")
+insert n ((Symbol_table(scT,nL,nA,sL)):rest) (ARGUMENT(str,t,dim)) 
+    | (in_index_list str sL) = error ("Symbol table error: " ++ str ++"is already defined.")   
+    | otherwise = (n,[Symbol_table(scT,nL,nA+1,(str,Var_attr(negate (nA+4),t,dim)):sL)])
+insert n ((Symbol_table(scT,nL,nA,sL)):rest) (VARIABLE (str,t,dim)) 
+    | (in_index_list str sL) =  error ("Symbol table error: "++ str ++"is already defined.")
+    | otherwise = (n,[Symbol_table(scT,nL+1,nA,(str,Var_attr(nL+1,t,dim)):sL)])
+insert n ((Symbol_table(scT,nL,nA,sL)):rest) (FUNCTION (str,ts,t))
+    | in_index_list str sL =  error ("Symbol table error: "++str++"is already defined.")
+    | otherwise = (n+1,(Symbol_table(scT,nL,nA,(str,Fun_attr(getlabel n "fn",ts,t)):sL)):rest)
+
+
+getlabel :: Int -> String -> String
+getlabel n s = s ++ "_" ++ show(n)
+
+    
+
+--helper function -- determines if a value is in a symbol table list
+in_index_list :: String -> [(String,SYM_VALUE)] -> Bool  
+in_index_list str [] = False
+in_index_list str ((x,_):xs)| str==x = True
+    | otherwise = in_index_list str xs
+ 
+ 
+    
+lookup: ST -> String -> SYM_I_DESC
+look_up s x = find 0 s where
 
 
 
-lookup:: ST -> string -> SYM_I_DESC
-return:: ST -> M_type
+found level (Var_attr(offset,t,dim)) =  I_VARIABLE(level,offset,t,dim)
+found level (Fun_attr(label,arg_Type,t)) = I_FUNCTION(level,label,arg_Type,t)
+found level (Con_attr (cnum, t, name)) = I_CONSTRUCTOR (cnum,t,name)
+found level (Type_attr s) = I_TYPE s
+			  
+found :: Int -> SYM_VALUE -> bool
+
+find_level :: [(String,SYM_VALUE)] -> Just v | Nothing
+find_level ((str,v):rest)|x == str = Just v
+					   |otherwise =  find_level rest
+find_level [] = Nothing
+
+
+find :: Int -> ST -> SYM_I_DESC 
+find n [] = error ("Could not find "++ str)
+find n (Symbol_table(_,_,_,vs)::rest) = 
+	 (case find_level vs of 
+	  Just v -> found n v
+  Nothing -> find (n+1) rest)      
+
+{-
+
+
+
+lookup: ST -> String -> SYM_I_DESC
+look_up s x = find 0 s where
+found level (Var_attr(offset,type,dim)) =  I_VARIABLE(level,offset,type,dim)
+found level (Fun_attr(label,arg_Type,type)) = I_FUNCTION(level,label,arg_Type,type)
+      ...
+
+find_level ((str,v):rest)|x== str = Just v
+					   |otherwise =  find_level rest
+find_level [] = Nothing
+
+find n [] = error ("Could not find "++ str)
+find n (Symbol_table(_,_,vs)::rest) = 
+	 (case find_level vs of 
+	  Just v -> found n v
+  Nothing -> find (n+1) rest)
+
+empty:: ST
+empty = []
+
+
+new_scope :: ScopeType -> ST -> ST
+newscope s = (Symbol_table(0,0,[])):s
+
+removeScope :: ScopeType -> ST -> ST
+
+ 
+return :: ST -> M_type
+-}
+
+
+
+--lookup:: ST -> string -> SYM_I_DESC
+--return:: ST -> M_type
